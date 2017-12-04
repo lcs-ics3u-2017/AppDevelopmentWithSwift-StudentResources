b0b37e4ea7c565603e9735a015c8817cab9a822c
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-04 13:35:48 -0500

    Initial Commit

---
+++ AppDevelopmentWithSwift-StudentResources/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppDevelopmentWithSwift-StudentResources
+//
+//  Created by Gordon, Russell on 2017-12-04.
+//  Copyright © 2017 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
